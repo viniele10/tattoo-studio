3b395f0b1a37714bc6f88fee390a6062118ece58
IplinskyLeo <leoipy16@gmail.com>
2022-09-19 02:55:01 -0300

    feat: estilização da página materiais

--- src/pages/Materiais/Materiais.jsx
+++ src/pages/Materiais/Materiais.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 
-// import { Link } from "react-router-dom";
-import HeaderTatuador from "../../components/Header/HeaderTatuador";
+import { Link } from "react-router-dom";
+import HeaderMateriais from "../../components/Header/HeaderMateriais";
 import Footer from "../../components/Footer/Footer";
+import './Materiais.css'
+
+function pesquisarMaterial() {
+  console.log("Materiais");
+}
 
 function Materiais() {
 
 
+
     return (
-      <div>
+      <div className="mainMateriais">
+        <header>
+          <HeaderMateriais tatuador="/#home" contato="/#home" />
+        </header>
+        <section className="inputPesquisar">
+          <strong>MATERIAIS</strong>
+          <input type="text"  name='pesquisar' id='pesquisar' onKeyUp={pesquisarMaterial()} placeholder="Pesquisar..."/>
+        </section>
 
-        <HeaderTatuador/>
+        <div className="parent">
+            <div className="div1"> </div>
+            <div className="div2"> </div>
+            <div className="div3"> </div>
+            <div className="div4"> </div>
+            <div className="div5"> </div>
+            <div className="div6"> </div>
+            <div className="div7"> </div>
+            <div className="div8"> </div>
+            <div className="div9"> </div>
+        </div>
 
-        <h1> Lista de materiais do estúdio:</h1>
-       
         <Footer/>
   
       </div>
